69b71c9dfb25b2afc27852fbb2f652a31168bbb4
Alexandra Noury <alexandranoury9@gmail.com>
2020-08-11 15:01:04 +0200

    MVC

---
+++ models/LivreManager.class.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "Model.class.php";
+require_once "Livre.class.php";
+
+class LivreManager extends Model{
+    private $livres;
+
+    public function ajoutLivre($livre){
+        $this->livres[] = $livre;
+    }
+
+    public function getLivres(){
+        return $this->livres;
+    }
+
+    public function chargementLivres() {
+        $req = $this->getBdd()->prepare("SELECT * FROM livre ORDER BY id DESC");
+        $req->execute();
+        $meslivres = $req->fetchAll(PDO::FETCH_ASSOC);
+        $req->closeCursor();
+
+        foreach ($meslivres as $livre) {
+        
+        
+            $l = new Livre($livre['id'], $livre['titre'], $livre['nbPages'], $livre['image'] );
+            //Genere les livres
+            $this->ajoutLivre($l);
+        }
+        
+        
+    }
+}
