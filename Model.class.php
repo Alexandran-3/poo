81bd925464f707b5b3430b1076c5860705a1a288
Alexandra Noury <alexandranoury9@gmail.com>
2020-08-11 13:06:22 +0200

    Ajout de la class Model Bdd

---
+++ Model.class.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class Model {
+
+    private static $pdo; // static pour qu'ils soit accessible par toute les class qui heritera de la class Model
+
+
+    private static  function setBdd() {
+        self::$pdo = new PDO("mysql:host=localhost;dbname=biblio_poo;charset=utf8", "root", "");
+        self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING).
+    }
+
+    protected function getBdd() {
+        if(self::$pdo === null) {
+            self::setBdd();
+        }
+        return self::$pdo;
+    }
+}
