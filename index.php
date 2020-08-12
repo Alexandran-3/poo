8686f246f9ffeb4d52b87ce215ac18199bb60a3b
Alexandra Noury <alexandranoury9@gmail.com>
2020-08-12 10:28:15 +0200

    constante pour acceder à toutes les route depuis la racine

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http"). // Permet de remplacer index.php par du vide. On test si on est sur https ou http le bon
+"://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]")); // http host qui est "localhost" PHP_self le chemin 
 
 require_once 'controllers/LivresController.controller.php';
 $livreController = new LivresController;
@@ -13,6 +15,7 @@ if(empty($_GET['page'])){
         case "livres" : $livreController->afficherLivres();
         break;
     }
+    var_dump($_SERVER[PHP_SELF]);
 
         
 }
