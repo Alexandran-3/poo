f6e5fba742aecf575707377984c2b37ea9081aaa
Alexandra Noury <alexandranoury9@gmail.com>
2020-08-12 10:47:08 +0200

    Deifinition des routes pour le crud

--- index.php
+++ index.php
@@ -5,17 +5,36 @@ define("URL", str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" :
 require_once 'controllers/LivresController.controller.php';
 $livreController = new LivresController;
 
-
+try{
 if(empty($_GET['page'])){
     require "views/accueil.view.php";
 } else {
-    switch($_GET['page']){
+
+    $url = explode("/", filter_var($_GET['page']), FILTER_SANITIZE_URL); // Decomposition url explose l'url à partir du /
+    // print_r($url);
+    switch($url[0]){
         case "accueil" : require "views/accueil.view.php";
         break;
-        case "livres" : $livreController->afficherLivres();
-        break;
-    }
-    var_dump($_SERVER[PHP_SELF]);
+        case "livres" : 
+        if(empty($url[1])) {
+            $livreController->afficherLivres();
+        } else if ($url[1] === "l"){
+            echo "affichage d'un livre"; 
+        } else if ($url[1] === "a"){
+            echo "Ajout d'un livre";
+        } else if ($url[1] === "m"){ // url pour modif livre "livres/m"
+            echo "Modifier un livre";
+        } else if ($url[1] === "s"){
+            echo "Supprimer un livre";
+        } else {
+            throw new Exception("La page n'existe pas");
+        }
 
-        
+        break;
+     }       
+  }
+}
+
+catch(Exception $e){
+    echo $e->getMessage();
 }
